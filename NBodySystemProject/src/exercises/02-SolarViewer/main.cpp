c6c8ece2ad4481f98c1770ef8bd73dbe0a969f67
fgmehlin <floran.gmehlin@gmail.com>
2012-11-17 17:20:26 +0100

    cleaned args

---
+++ NBodySystemProject/src/exercises/02-SolarViewer/main.cpp
@@ -0,0 +1,27 @@
+//=============================================================================
+//                                                                            
+//   Exercise code for the lecture
+//                                                                            
+//=============================================================================
+
+#include "SolarViewer.h"
+
+
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	
+	SolarViewer window("Solar Viewer", 800, 800);
+	
+	if(argc > 1)
+	{
+		window.load_mesh(argv[1], STARS);
+	}
+	if(argc > 2)
+	{
+		window.load_mesh(argv[2], SUN);
+	}
+		
+	glutMainLoop();
+}
